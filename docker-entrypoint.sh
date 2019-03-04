02220a1e9f6cb83e6c28da03502d7d4d71e92fa8
Andrew Johnson <blackandred@users.noreply.github.com>
2019-03-04 21:19:19 +0100

    Added support for read-only ssh keys as volume

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,7 +1,21 @@
 #!/bin/bash
 
+#
+# Option to mount read-only id_rsa and id_rsa.pub on the root directory
+# so the files will be copied to proper place and permissions will be corrected
+#
+# Reason: Each container run on original files would change permissions (chmod), that would charm git repository
+#
+if [[ -f /id_rsa ]] && [[ -f /id_rsa.pub ]]; then
+    mkdir -p /home/revproxy/.ssh
+    cp /id_rsa /home/revproxy/.ssh/
+    cp /id_rsa.pub /home/revproxy/.ssh/
+    chown -R revproxy:revproxy /home/revproxy/.ssh
+    chmod 700 /home/revproxy/.ssh/id_rsa
+fi
+
 echo " >> Correcting permissions"
-chown -R revproxy:revproxy /home/revproxy
+chown -R revproxy:revproxy /home/revproxy 2>/dev/null
 
 echo " >> Adding hosts to known hosts first time, if there are not all added"
 su revproxy -c "/rn/bin/add-to-known-hosts.sh"
